5672ff41b04db1669c137890f4aa66e3de8fd39b
TAG <akhil.thallapelly@cdk.com>
2018-03-16 02:43:54 +0530

    added postbook

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var cors = require('cors');
-app.use(cors());
+
 
 app.use(bodyParser.json());
 
