e909683f303535629ce2cdb61a3702fa24e91118
TAG <akhil.thallapelly@cdk.com>
2018-03-15 20:25:19 +0530

    added postbook fun

--- models/bookinfo.js
+++ models/bookinfo.js
@@ -48,7 +48,7 @@ var bookInfoSchema = mongoose.Schema({
     }
 });
 
-var BookInfo = module.exports = mongoose.model('bookinfos', bookInfoSchema);
+var BookInfo = module.exports = mongoose.model('bookinfo', bookInfoSchema);
 
 
 module.exports.getBooks = function (callback, limit) {
