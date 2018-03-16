66d14c41c617232134024ec620bc5c9dbf2753ab
TAG <akhil.thallapelly@cdk.com>
2018-03-16 12:48:27 +0530

    adding cors

--- app.js
+++ app.js
@@ -145,10 +145,10 @@ db.once('open', function () {
     });
 
     app.get('/', function (req, res) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-        res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+        // res.setHeader('Access-Control-Allow-Origin', '*');
+        // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+        // res.setHeader('Access-Control-Allow-Credentials', true); // If needed
         res.send("Hey TAG!");
     });
 
