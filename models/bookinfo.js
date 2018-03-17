948efeeafb933dd451eff691212e896cd87c83d2
TAG <akhil.thallapelly@cdk.com>
2018-03-17 11:58:16 +0530

    login

--- models/bookinfo.js
+++ models/bookinfo.js
@@ -48,63 +48,76 @@ var bookInfoSchema = mongoose.Schema({
     }
 });
 
-var bookTotalDetails = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    publisher: {
-        type: String,
-        required: true
-    },
-    edition: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    uid: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    isAcademic: {
-        type: String,
-        required: true
-    },
-    created_date: {
-        type: Date,
-        default: null
-    },
-    updated_date: {
-        type: Date,
-        default: null
-    },
-    bookImages: [{ type: Schema.Types.ObjectId, ref: 'BookImage' }],
-    bookContact: [{ type: Schema.Types.ObjectId, ref: 'BookContact' }],
-    bookAcademic: [{ type: Schema.Types.ObjectId, ref: 'BookAcademic' }],
-})
+// var bookTotalDetails = mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     author: {
+//         type: String,
+//         required: true
+//     },
+//     publisher: {
+//         type: String,
+//         required: true
+//     },
+//     edition: {
+//         type: String,
+//         required: true
+//     },
+//     price: {
+//         type: Number,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     uid: {
+//         type: String,
+//         required: true
+//     },
+//     status: {
+//         type: String,
+//         required: true
+//     },
+//     isAcademic: {
+//         type: String,
+//         required: true
+//     },
+//     created_date: {
+//         type: Date,
+//         default: null
+//     },
+//     updated_date: {
+//         type: Date,
+//         default: null
+//     },
+//     bookImages: [{ type: Schema.Types.ObjectId, ref: 'BookImage' }],
+//     bookContact: [{ type: Schema.Types.ObjectId, ref: 'BookContact' }],
+//     bookAcademic: [{ type: Schema.Types.ObjectId, ref: 'BookAcademic' }],
+// });
 
 var BookInfo = module.exports = mongoose.model('bookinfo', bookInfoSchema);
 
-var BookTotalInfo = module.exports = mongoose.model('bookinfo', bookTotalDetails);
+// var BookTotalInfo = module.exports = mongoose.model('bookinfo', bookTotalDetails);
 
 
 module.exports.getBooks = function (callback, limit) {
-    BookTotalInfo.find(callback).limit(limit);
+    // BookTotalInfo.find(callback).limit(limit);
+    BookInfo.aggregate([
+        { $lookup:
+           {
+             from: 'bookcontact',
+             localField: '_id',
+             foreignField: 'bookId',
+             as: 'orderdetails'
+           }
+         }
+        ]).toArray(function(err, res) {
+        if (err) throw err;
+        callback(res);
+      });
 }
 
 module.exports.getBookById = function (id,callback) {
